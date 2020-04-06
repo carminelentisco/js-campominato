f4269878642a267b576e0d4beca32ebacc61d76f
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-06 15:12:05 +0200

    JS
    Indicazione esercizio

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+/**************************************************************************************************************************************************\ 
+ * 
+ * ESERCIZIO ---> CAMPO MINATO  
+ * 
+ * Esecuzione : 
+ * 
+ *  1) Il computer deve generare 16 numeri casuali tra 1 e 100 (numeri vietati).
+ *  2) Chiedere all’utente di inserire un numero alla volta, compreso tra 1 e 100.
+ *  
+ * ATTENZIONE :  L’utente non può inserire più volte lo stesso numero.
+ *  
+ *  3) La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+ *  3.1) Altrimenti si continua chiedendo all’utente un altro numero.
+ * 
+ *  4)Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+ * 
+\************************************************************************************************************************************************/
+
